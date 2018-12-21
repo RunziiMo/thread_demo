684de251362665b9fba50bd482a7bbc40632381c
runziimo <runziimo@gmail.com>
2018-12-21 15:21:57 +0800

    demo to show multi thread

--- main.cpp
+++ main.cpp
@@ -16,15 +16,32 @@ int main(int argc, char **argv) {
   }).join();
   std::cout << "Hello, World! after join" << std::endl;
 
-  auto thread2 = std::thread([]() {
-    int count = 0;
-    while (count++ <= 10) {
+  bool is_running = true;
+  int mode = -1;
+
+  auto thread2 = std::thread([&mode, &is_running]() {
+    while (is_running) {
       // todo(jingjing) you can add XP::serialport_write and XP::serialport_read function here
+      // todo(jingjing) when read serial done change mode type
       usleep(1000);
-      std::cout << "not join while " << count << std::endl;
     }
   });
   std::cout << "Hello, World! after not join" << std::endl;
-  thread2.join();
+
+  auto thread_main = std::thread([&mode, &is_running]() {
+    while (is_running) {
+      if (mode == 1) {
+        // todo(jingjing) record data
+        std::cout << "start record data" << std::endl;
+      } else if (mode == 2) {
+        // todo (jingjing) start navigation
+        std::cout << "start navigation" << std::endl;
+      } else {
+        std::cout << "mode is still -1, please choose mode" << std::endl;
+      }
+      usleep(1000000);
+    }
+  });
+  thread_main.join();
   return 0;
 }
